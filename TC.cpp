0689e2f4c6135ee4b1ca39c60412d12393524cf2
PranavKarne <karnepranav63882@gmail.com>
2025-04-14 22:47:29 +0530

    daily update: 2025-04-14

---
+++ TC.cpp
@@ -0,0 +1,16 @@
+/*
+in 1sec we have 10^8 operations generally
+
+for ex: 
+
+in a problem its given like this
+time limit per test: 2 secs
+1 <= t <= 1000 
+
+
+tht means 1 test case allows 2 secs so total 2*10^8 operations
+so the operations per test case are 2*10^8 / 10^3 = 2 * 10^5 number of operations per test case
+
+
+
+*/ 
