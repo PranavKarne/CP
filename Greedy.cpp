1eccd241dbe61d8e232d755dfe1fb0588dc709c5
PranavKarne <karnepranav63882@gmail.com>
2025-04-17 11:36:04 +0530

    daily update: 2025-04-17

---
+++ Greedy.cpp
@@ -0,0 +1,17 @@
+/*
+
+1.for every single Greedy Problem there exists a brute force solution
+2. if u are able to make certain obervations then might u 
+3.there are no standard greedy algos 
+
+// note down from Video's Transcript
+
+
+
+How to prove a Greedy Strategy
+1. formal or intuitive Proof(some mathematical eqns)
+2. try out so many 
+
+
+*/
+
