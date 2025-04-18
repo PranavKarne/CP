29eeb204bb3b580316cc92f751171097a7e4afc4
PranavKarne <karnepranav63882@gmail.com>
2025-04-18 22:40:18 +0530

    daily update: 2025-04-18

--- Greedy.cpp
+++ Greedy.cpp
@@ -1,16 +1,36 @@
 /*
 
 1.for every single Greedy Problem there exists a brute force solution
-2. if u are able to make certain obervations then might u 
-3.there are no standard greedy algos 
+2.when u make certain observations then u think of greedy solns , when u prove certain ideas
+3.there is no standard greedy algos 
+4.Greedy Strategy = A strategy that assumes that the best answer can be found using some
+(not all) possibilities and only tries out those limited possibilities
+ It involves coming up with a claim (greedy) and then proving it
 
-// note down from Video's Transcript
+5.to prove your Greedy Strategy 
+  --> u have some mathematical equations / formal or intuituive proof
+  --> Trying out too many cases and failing to disprove
+
+6.u come up with a greedy strategy and u tryout multiple cases , even if one the cases u r greedy startegy 
+does not give up best answer , then your strategy is wrong 
+
+7. u r greedy strategy shld work for almost all cases , in any of the case it fails , it must not be your algo , it must be 
+where u r strategy cannot be applied then tht is ok , if on applying , when it can be  applied , it fails it means the strategy 
+is wrong
+
+8.if we say tht we tried out all the possible cases and the strategy worked on all of them , then it is the correct strategy
+
+9.  for formal proof , we have to write mathematical eqns and tht is how u prove it
+10. Greedy is subjective , not all problems are intuitive for everyone
+11. we dont have to try out all the cases , we'll have a strategy tht works 
+12. in cp problems , if the order of the elements doesnt matter then sort the array/vector and then start thinking  abt the problem this 
+for any CP problem
+13. 
 
 
 
-How to prove a Greedy Strategy
-1. formal or intuitive Proof(some mathematical eqns)
-2. try out so many 
+
+
 
 
 */
