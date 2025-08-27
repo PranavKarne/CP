16302c4aef0d14cf4fa3f869e121f111742f2004
PranavKarne <karnepranav63882@gmail.com>
2025-08-27 22:07:47 +0530

    daily update: 2025-08-27

--- CP31sheet.cpp
+++ CP31sheet.cpp
@@ -1,19 +1,17 @@
 /*
-900 Rated 
-
-26. Odd Divisor = https://codeforces.com/problemset/problem/1475/A
-
-
-
 
 */
 
 /* 1000 */
 
-// 27. Fair Numbers - July 7th 
+// 27. Fair Numbers - https://codeforces.com/contest/1411/problem/B
 
 // see the maximum Tc possible , not every soln is O(n2) , check the operations per test case 
 // and think of the approach 
+
+
+
+
 
 
 
