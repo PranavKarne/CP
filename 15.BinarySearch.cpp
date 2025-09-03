2544f36b2391c4ecb410a18bd6f76c084558cb00
PranavKarne <karnepranav63882@gmail.com>
2025-09-03 22:17:34 +0530

    daily update: 2025-09-03

--- 15.BinarySearch.cpp
+++ 15.BinarySearch.cpp
@@ -1,33 +1,10 @@
-#include <iostream>
-#include <vector>
+#include<bits/stdc++.h>
 using namespace std;
 
-// Binary Search Function
-int binarySearch(const vector<int>& arr, int target) {
-    int left = 0, right = arr.size() - 1;
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (arr[mid] == target) {
-            return mid; // Target found
-        } else if (arr[mid] < target) {
-            left = mid + 1; // Search in the right half
-        } else {
-            right = mid - 1; // Search in the left half
-        }
-    }
-    return -1; // Target not found
+int main()
+{
+
+
+
 }
 
-int main() {
-    vector<int> arr = {1, 3, 5, 7, 9, 11};
-    int target = 7;
-
-    int result = binarySearch(arr, target);
-    if (result != -1) {
-        cout << "Element found at index: " << result << endl;
-    } else {
-        cout << "Element not found" << endl;
-    }
-
-    return 0;
-}
