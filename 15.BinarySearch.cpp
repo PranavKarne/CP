cd97807ca72ee29c7267020785b98144daa6b203
PranavKarne <karnepranav63882@gmail.com>
2025-09-06 12:08:20 +0530

    daily update: 2025-09-06

--- 15.BinarySearch.cpp
+++ 15.BinarySearch.cpp
@@ -6,5 +6,9 @@ int main()
 
 
 
+
 }
 
+// lower_bound → points to the first element ≥ target.
+
+// upper_bound → points to the first element > target.
---
+++ pranav.cpp
@@ -0,0 +1,41 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // partition array by sign(-ve,+ve)
+
+    
+    
+
+    vector<int> v1 = {1, 2, 3};
+    vector<int> v2 = {4, 5, 6};
+
+    
+
+    //v2 now has all elements
+    v2.insert(v2.end(),v1.begin(),v1.end());
+    for(int y : v2) cout << y << " ";
+    cout << endl; 
+
+
+
+
+    cout << "example 2" << endl;
+    vector<int>karne = {7,8,9};
+    vector<int>pk = {10,11,12};
+
+    karne.insert(karne.end(),pk.begin(),pk.end());
+    for(int p : karne) cout << p << " ";
+    cout << endl;
+
+
+
+
+
+
+    
+
+    
+
+
+}
