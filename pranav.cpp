60c9f5e5ac0053cdf987f5f568ecb070a9232817
PranavKarne <karnepranav63882@gmail.com>
2025-09-07 17:54:36 +0530

    daily update: 2025-09-07

--- pranav.cpp
+++ pranav.cpp
@@ -1,41 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
+
+int digitsum(int n)
+{
+    int sum = 0;
+    while(n > 0)
+    {
+        int ld = n % 10;
+        sum += ld;
+        n = n / 10; 
+    }
+    return sum;
+}
+
+
 int main()
 {
-    // partition array by sign(-ve,+ve)
+    int n; cin >> n;
+    int target = 2 * digitsum(n);
 
+    int x = n + 1;
+    while(true)
+    {
+        if(digitsum(x) == target)
+        {
+            cout << x << endl;
+            break;
+        }
+        x++;
+        
+
+    }
     
     
-
-    vector<int> v1 = {1, 2, 3};
-    vector<int> v2 = {4, 5, 6};
-
-    
-
-    //v2 now has all elements
-    v2.insert(v2.end(),v1.begin(),v1.end());
-    for(int y : v2) cout << y << " ";
-    cout << endl; 
-
-
-    cout << "example 2" << endl;
-    vector<int>karne = {7,8,9};
-    vector<int>pk = {10,11,12};
-
-    karne.insert(karne.end(),pk.begin(),pk.end());
-    for(int p : karne) cout << p << " ";
-    cout << endl;
-
-
-
-
-
-
-
-
-    
-
-    
-
-
 }
