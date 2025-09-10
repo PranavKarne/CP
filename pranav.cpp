dd29964e51cb9e203e18b5a41f43b37933ba11e8
PranavKarne <karnepranav63882@gmail.com>
2025-09-10 20:33:53 +0530

    daily update: 2025-09-10

---
+++ pranav.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string num;
+    cin >> num;
+
+    int i = 0, j = num.size() - 1;
+    while (i <= j) 
+    {
+        if ((num[i] == '0' && num[j] == '0') ||
+            (num[i] == '1' && num[j] == '1') ||
+            (num[i] == '8' && num[j] == '8') ||
+            (num[i] == '6' && num[j] == '9') ||
+            (num[i] == '9' && num[j] == '6')) {
+            i++;
+            j--;
+        } else {
+            cout << "false\n";
+            return 0;
+        }
+    }
+
+    cout << "true\n";
+    return 0;
+}
